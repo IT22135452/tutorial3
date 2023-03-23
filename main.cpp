282bd766229012351eb7e8fcecc6f9b9556be9ed
IT22135452 <127375300+IT22135452@users.noreply.github.com>
2023-03-23 06:06:05 +0000

    exe4

--- main.cpp
+++ main.cpp
@@ -89,20 +89,30 @@ int volume(int height, int width, int length){
   return height*width*length;
 }
 */
+
 #include <iostream>
 using namespace std;
 
 void print(int len, int wth);
-void input(int len, int wth);
+void input(int &len, int &wth);
 // Do not change the main() function
 int main() {
  int length = 10, width = 5;
+  
  input(length, width);
  print(length, width);
- return 0;
+  
+ return 0; 
 }
-// Do not change the print() function
+
 void print(int len, int wth) {
  cout << "Length : " << len
  << ", Width : " << wth << endl;
+}
+
+void input(int &len, int &wth){
+  cout << "Enter length: ";
+  cin>> len;
+  cout<< "Enter width: ";
+  cin>> wth; 
 }
